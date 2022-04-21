89bc1428f849cf83f812e330b563e922cac43aa9
Karthikeya <yelisettikarthik0@gmail.com>
2022-04-22 02:11:53 +0530

    Decided to go with single page + multipage architecture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#/">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#/">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#/about">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#/contact">Contact</a>
-                </li>
-            </ul>
+    <div className='Navbar'>
+        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Navbar</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
     </div>
   )
